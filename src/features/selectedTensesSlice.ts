ba79efa2365bcf909d53339a777a5891ab497ee9
Konstantin Vasin <k.vasin@free-lance.ru>
2024-11-30 13:02:27 +0100

    CME-6: add store file for tenses

---
+++ src/features/selectedTensesSlice.ts
@@ -0,0 +1,30 @@
+// features/selectedTensesSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  selectedTenses: [],
+};
+
+const selectedTensesSlice = createSlice({
+  name: "selectedTenses",
+  initialState,
+  reducers: {
+    setSelectedTenses: (state, action) => {
+      state.selectedTenses = action.payload;
+    },
+    addTense: (state, action) => {
+      if (!state.selectedTenses.includes(action.payload)) {
+        state.selectedTenses.push(action.payload);
+      }
+    },
+    removeTense: (state, action) => {
+      state.selectedTenses = state.selectedTenses.filter(
+        (tense) => tense !== action.payload,
+      );
+    },
+  },
+});
+
+export const { setSelectedTenses, addTense, removeTense } =
+  selectedTensesSlice.actions;
+export default selectedTensesSlice.reducer;
