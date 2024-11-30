cb768dc613b0f53c5542ddec174cdbd381080a08
Konstantin Vasin <k.vasin@free-lance.ru>
2024-11-30 12:45:00 +0100

    CME-6: add style for UiChooseList shared common

--- src/components/UiChooseList.tsx
+++ src/components/UiChooseList.tsx
@@ -18,10 +18,32 @@ const UiChooseList = ({ options, selectedOptions, onChange }: Props) => {
   const isActive = (item: string) => selectedOptions.includes(item);
 
   return (
-    <ul>
+    <ul className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-4">
       {(options as any).map((option: string, index: number) => (
-        <li key={`Choose-list_${index}`} onClick={() => handleChange(option)}>
+        <li
+          key={`Choose-list_${index}`}
+          className="p-4 bg-gray-200 rounded-lg shadow-md flex items-center cursor-pointer"
+          onClick={() => handleChange(option)}
+        >
           {option}
+          {isActive(option) && (
+            <span className="ml-2">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth={1.5}
+                stroke="currentColor"
+                className="size-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m4.5 12.75 6 6 9-13.5"
+                />
+              </svg>
+            </span>
+          )}
         </li>
       ))}
     </ul>
