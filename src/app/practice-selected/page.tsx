3b583d60a65ca67fa197ea87c94183399c570f41
Konstantin Vasin <randomhero177@gmail.com>
2024-12-28 12:02:29 +0000

    set up random choose of tense, verb and pronoun

--- src/app/practice-selected/page.tsx
+++ src/app/practice-selected/page.tsx
@@ -7,6 +7,7 @@ import Pronomb from "@/data/pronomb";
 import getRandomInRange from "@/util/getRandom";
 
 export default function PracticeSelected() {
+    const SpanishVerbs = require("spanish-verbs");
     const dispatch = useDispatch();
     const selectedVerbs = useSelector(
         (state: RootState) => state.selectedVerbs.selectedVerbs,
@@ -16,7 +17,16 @@ export default function PracticeSelected() {
         (state: RootState) => state.selectedTenses.selectedTenses,
     );
 
-    const [currentPronomb, setCurrentPronomb] = useState('')
+    const [currentPronomb, setCurrentPronomb] = useState('');
+    const [currentVerb, setCurrentVerb] = useState('');
+    const [currentTense, setCurrentTense] = useState('');
+
+    function getCurrentCorrect() {
+        console.log(currentPronomb);
+        const conjugatedVerb = SpanishVerbs.getConjugation(currentVerb, currentTense.replace(' ', '_'), Pronomb.indexOf(currentPronomb));
+        console.log(conjugatedVerb)
+        return conjugatedVerb
+    }
 
     useEffect(() => {
         console.log('asdasda asdasd 2');
@@ -25,15 +35,23 @@ export default function PracticeSelected() {
         console.log(selectedTenses);
         console.log(selectedTenses.length);
         console.log(Pronomb);
+
+        setCurrentVerb(selectedVerbs.length > 1 ? selectedVerbs[getRandomInRange(0, selectedVerbs.length - 1)] : selectedVerbs[0]);
+        setCurrentTense(selectedTenses.length > 1 ? selectedTenses[getRandomInRange(0, selectedTenses.length - 1)] : selectedTenses[0]);
+
+
         setCurrentPronomb(Pronomb[getRandomInRange(0, 5)]);
       }, []);
 
     return (
         <div>
-        a
-        <div>{selectedVerbs}</div>
-        <div>asd</div>
-                {currentPronomb}
+            a
+            <div>{selectedVerbs}</div>
+            <div>asd</div>
+            {currentPronomb}
+            <div>{currentTense}</div>
+            <div>{currentVerb}</div>
+            <div><span onClick={() => getCurrentCorrect()}>get</span></div>
         </div>
     )
 }
--- src/data/pronomb.ts
+++ src/data/pronomb.ts
@@ -1,10 +1,10 @@
-const Pronomb: Record<number, string> = {
-    0: 'Yo',
-    1: 'Tu',
-    2: 'El/Ella',
-    3: 'Nosotros',
-    4: 'Vosotros',
-    5: 'Ellos'
-}
+const Pronomb: string[] = [
+    'Yo',
+    'Tu',
+    'El/Ella',
+    'Nosotros',
+    'Vosotros',
+    'Ellos'
+]
 
 export default Pronomb
