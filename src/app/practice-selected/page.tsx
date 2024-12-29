6dfc1ee5b729a475eeadde59b1f08d4d785d6d94
Konstantin Vasin <randomhero177@gmail.com>
2024-12-29 10:19:01 +0000

    set tiny styles and randomiseVerbsAndTense

--- src/app/practice-selected/page.tsx
+++ src/app/practice-selected/page.tsx
@@ -21,37 +21,42 @@ export default function PracticeSelected() {
     const [currentVerb, setCurrentVerb] = useState('');
     const [currentTense, setCurrentTense] = useState('');
 
-    function getCurrentCorrect() {
+    function getCurrentConjugated() {
         console.log(currentPronomb);
         const conjugatedVerb = SpanishVerbs.getConjugation(currentVerb, currentTense.replace(' ', '_'), Pronomb.indexOf(currentPronomb));
         console.log(conjugatedVerb)
         return conjugatedVerb
     }
 
-    useEffect(() => {
-        console.log('asdasda asdasd 2');
-        console.log(selectedVerbs);
-        console.log(selectedVerbs.length);
-        console.log(selectedTenses);
-        console.log(selectedTenses.length);
-        console.log(Pronomb);
-
+    function randomiseVerbsAndTense() {
         setCurrentVerb(selectedVerbs.length > 1 ? selectedVerbs[getRandomInRange(0, selectedVerbs.length - 1)] : selectedVerbs[0]);
         setCurrentTense(selectedTenses.length > 1 ? selectedTenses[getRandomInRange(0, selectedTenses.length - 1)] : selectedTenses[0]);
-
-
         setCurrentPronomb(Pronomb[getRandomInRange(0, 5)]);
+    }
+
+    useEffect(() => {
+        randomiseVerbsAndTense();
       }, []);
 
     return (
         <div>
-            a
-            <div>{selectedVerbs}</div>
-            <div>asd</div>
-            {currentPronomb}
-            <div>{currentTense}</div>
-            <div>{currentVerb}</div>
-            <div><span onClick={() => getCurrentCorrect()}>get</span></div>
+            <div>   
+                {selectedVerbs.map((item) => (
+                    <span key={`selectedVerbs_item_${item}`}>{item} </span>
+                ))}
+            </div>
+            
+            <div>{currentPronomb} <i><b>{currentVerb}</b></i> in {currentTense}</div>
+            <div className="mt-6">
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    onClick={() => getCurrentConjugated()}
+                >see result</button>
+                <button
+                    className="ml-4 px-6 py-2 bg-green-500 text-white font-bold rounded-lg shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50 transition"
+                    onClick={() => randomiseVerbsAndTense()}
+                >next</button>
+            </div>
         </div>
     )
 }
