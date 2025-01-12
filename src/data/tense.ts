fb8c94e8f9100473b3ad3531f94989b0b76e63f1
Konstantin Vasin <k.vasin@free-lance.ru>
2025-01-12 23:26:04 +0100

    CME-26: add dictionary tenses mapping

---
+++ src/data/tense.ts
@@ -0,0 +1,21 @@
+const Tense = {
+  INDICATIVE_PRESENT: "Presente",
+  INDICATIVE_IMPERFECT: "Pretérito imperfecto",
+  INDICATIVE_PRETERITE: "pretérito simple",
+  INDICATIVE_FUTURE: "futuro",
+  INDICATIVE_PERFECT: "pretérito perfecto",
+  INDICATIVE_PLUPERFECT: "pluscuamperfecto",
+  INDICATIVE_FUTURE_PERFECT: "futuro perfecto",
+  INDICATIVE_PRETERITE_PERFECT: "pretérito anterior",
+  SUBJUNCTIVE_PRESENT: "Subjuntivo presente",
+  SUBJUNCTIVE_IMPERFECT_RA: "Subjuntivo pretérito imperfecto 1(RA)",
+  SUBJUNCTIVE_IMPERFECT_SE: "Subjuntivo pretérito imperfecto 2 (SE)",
+  SUBJUNCTIVE_FUTURE: "Subjuntivo futuro",
+  SUBJUNCTIVE_PERFECT: "Subjuntivo pretérito perfecto",
+  SUBJUNCTIVE_PLUPERFECT: "Subjuntivo pretérito pluscuamperfecto",
+  SUBJUNCTIVE_FUTURE_PERFECT: "Subjuntivo futuro perfecto",
+  CONDITIONAL_PRESENT: "Condicional simple",
+  CONDITIONAL_PERFECT: "Condicional compuesto",
+};
+
+export default Tense;
