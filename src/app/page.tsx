b689bbb758e295a78a76ee2c8629c243a87e821b
Konstantin Vasin <k.vasin@free-lance.ru>
2025-03-22 13:45:24 +0100

    CME-27: better text and style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <div className="max-w-4xl grid gap-8 sm:grid-cols-1 md:grid-cols-3">
         <Link href="/all-verbs" className="group">
           <div className="p-6 bg-white shadow-lg rounded-2xl flex flex-col items-center transition-transform transform hover:scale-105">
-            <div className="mb-4 w-14 h-14 flex items-center justify-center text-xl font-semibold text-white bg-gray-700 rounded-full">
+            <div className="mb-4 w-14 h-14 flex items-center justify-center text-xl font-semibold text-white bg-gray-600 rounded-full transition-colors duration-300 group-hover:bg-gray-900">
               1
             </div>
             <h2 className="text-xl font-semibold text-gray-700 mb-2">
@@ -24,7 +24,7 @@ export default function HomePage() {
 
         <Link href="/choose-tense" className="group">
           <div className="p-6 bg-white shadow-lg rounded-2xl flex flex-col items-center transition-transform transform hover:scale-105">
-            <div className="mb-4 w-14 h-14 flex items-center justify-center text-xl font-semibold text-white bg-gray-700 rounded-full">
+            <div className="mb-4 w-14 h-14 flex items-center justify-center text-xl font-semibold text-white bg-gray-600 rounded-full transition-colors duration-300 group-hover:bg-gray-900">
               2
             </div>
             <h2 className="text-xl font-semibold text-gray-700 mb-2">
@@ -39,7 +39,7 @@ export default function HomePage() {
 
         <Link href="/practice-selected" className="group">
           <div className="p-6 bg-white shadow-lg rounded-2xl flex flex-col items-center transition-transform transform hover:scale-105">
-            <div className="mb-4 w-14 h-14 flex items-center justify-center text-xl font-semibold text-white bg-gray-700 rounded-full">
+            <div className="mb-4 w-14 h-14 flex items-center justify-center text-xl font-semibold text-white bg-gray-600 rounded-full transition-colors duration-300 group-hover:bg-gray-900">
               3
             </div>
             <h2 className="text-xl font-semibold text-gray-700 mb-2">
