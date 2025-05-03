3bf4b972f26b93300b6812ef671a0558f2069218
Konstantin Vasin <k.vasin@free-lance.ru>
2025-05-03 17:10:49 +0200

    CME-14: start main menu refactoring

--- src/components/MainMenu.tsx
+++ src/components/MainMenu.tsx
@@ -1,20 +1,30 @@
 import Link from "next/link";
 import { PagesUrl } from "@/data/urls";
+import { CircleUserRound } from "lucide-react";
 
 const MainMenu = () => {
   return (
     <div className="flex justify-center">
-      <Link href={PagesUrl.home} className="p-4">
-        Home
+      <Link href={PagesUrl.home} className="p-4 me-auto">
+        <img
+          src="/CME_logo_book.png"
+          alt="Logo"
+          style={{ width: "200px", height: "auto" }}
+        />
       </Link>
-      <Link href={PagesUrl.register} className="p-4">
-        registration
-      </Link>
-      <Link href={PagesUrl.lang} className="p-4">
+
+      {/*<Link href={PagesUrl.lang} className="p-4">
         Choose lang templ
-      </Link>
+      </Link>*/}
       <Link href={PagesUrl.verbs} className="p-4">
         All verbs
+      </Link>
+      <Link href={PagesUrl.tense} className="p-4">
+        Select tenses
+      </Link>
+      <Link href={PagesUrl.register} className="p-4 ms-auto">
+        registration
+        <CircleUserRound />
       </Link>
     </div>
   );
