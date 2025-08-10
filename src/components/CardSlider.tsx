2baef1f0441fff2f53226028b200c47928fd676e
randomhero177 <randomhero177@gmail.com>
2025-08-10 13:04:31 +0200

    CME-38: small style fix

--- src/components/CardSlider.tsx
+++ src/components/CardSlider.tsx
@@ -1,17 +1,40 @@
 "use client";
-import React from "react";
+import { useEffect, useCallback, useState } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 
 export function CardSlider() {
-  const [emblaRef] = useEmblaCarousel({ loop: true });
+  const [isMobile, setIsMobile] = useState(false);
+  const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true });
+  const scrollPrev = useCallback(() => {
+    if (emblaApi) emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    if (emblaApi) emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  useEffect(() => {
+    console.log("hey");
+    const checkMobile = () => setIsMobile(window.innerWidth < 768);
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
 
   return (
-    <div className="embla" ref={emblaRef}>
+    <div className="overflow-hidden" ref={isMobile ? emblaRef : null}>
       <div className="embla__container">
         <div className="embla__slide">Slide 1</div>
         <div className="embla__slide">Slide 2</div>
         <div className="embla__slide">Slide 3</div>
       </div>
+      <button className="embla__prev" onClick={scrollPrev}>
+        Prev
+      </button>
+      <button className="embla__next" onClick={scrollNext}>
+        Next
+      </button>
     </div>
   );
 }
