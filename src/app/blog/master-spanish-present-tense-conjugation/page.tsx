3c7b3c5af121718daa60e56894d93afe666862d6
randomhero177 <randomhero177@gmail.com>
2025-08-31 11:24:20 +0200

    CME-49: create blog structure and add first seo article

---
+++ src/app/blog/master-spanish-present-tense-conjugation/page.tsx
@@ -0,0 +1,21 @@
+export default function ArticleMasterPresent() {
+  return (
+    <div className="">
+      <article>
+        <h1>How to Master Spanish Present Tense Conjugations</h1>
+      </article>
+      <div>
+        <p>
+          Learning Spanish can feel overwhelming at first—especially when you
+          realize that verbs change depending on the subject. But don’t worry:
+          once you understand the{" "}
+          <strong>patterns of present tense conjugations</strong>, you’ll unlock
+          the foundation of Spanish communication.
+          <br /> <br /> In this guide, we’ll explain how Spanish present tense
+          works, cover the most common conjugation endings, and give you
+          practical tips to master them quickly.
+        </p>
+      </div>
+    </div>
+  );
+}
