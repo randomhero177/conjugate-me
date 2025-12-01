8b3a0efa462fe350ac67b9bbccf34696dc9d8159
randomhero177 <randomhero177@gmail.com>
2025-12-02 00:41:46 +0100

    [cme-61]: add checkbox

--- src/components/VerbsPresetChip.tsx
+++ src/components/VerbsPresetChip.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { mergeSelectedVerbs } from "@/store/modules/selectedVerbsSlice";
 import { useDispatch, useSelector } from "react-redux";
 import * as ga from "@/plugins/ga";
@@ -25,10 +26,25 @@ const VerbsPresets = ({ verbConfig }: Props) => {
     dispatch(mergeSelectedVerbs(verbsList));
   }
 
+  const [checked, setChecked] = useState(false);
+  const id = `preset-${verbConfig.key}`;
+
   return (
     <div>
       <div onClick={() => choosePreset(verbConfig)}>
-        {verbConfig.key.toUpperCase()}
+        <label
+          htmlFor={id}
+          className="flex items-center gap-3 p-3 rounded-xl cursor-pointer transition"
+        >
+          <input
+            id={id}
+            type="checkbox"
+            checked={checked}
+            onChange={() => setChecked(!checked)}
+            className="h-5 w-5 rounded border-gray-300 text-blue-600 focus:ring-blue-500 cursor-pointer"
+          />
+          <span>{verbConfig.key.toUpperCase()}</span>
+        </label>
       </div>
     </div>
   );
