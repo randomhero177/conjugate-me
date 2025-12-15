6f9a9ff1a269e73f76faa3e4f4ae6d471ead88c3
randomhero177 <randomhero177@gmail.com>
2025-12-15 12:20:51 +0100

    [cme-61]: metadata for pages

---
+++ src/app/all-verbs/AllVerbsClient.tsx
@@ -0,0 +1,145 @@
+"use client";
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  addVerb,
+  removeVerb,
+  setSelectedVerbs,
+} from "@/store/modules/selectedVerbsSlice";
+import ChooseStepsDescription from "@/app/all-verbs/components/ChooseStepsDescription";
+import UiCombobox from "@/components/UiCombobox";
+import Dictionary from "@/data/dictionaryNoReflexiv";
+import { useRouter } from "next/navigation";
+import { RootState } from "@/types/typeVerbs";
+import { PagesUrl } from "@/data/urls";
+import VerbsPresets from "@/components/VerbsPresets";
+import FooterAction from "@/components/FooterAction";
+import ChipItem from "@/components/ChipItem";
+import ClearAllVerbs from "@/components/ClearAllVerbs";
+import * as ga from "@/plugins/ga";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Practice Spanish Conjugations | Conjugate Me",
+  description: "Practice Spanish verb conjugation by tense and person.",
+};
+
+export default function AllVerbsPage() {
+  const router = useRouter();
+  const selectedVerbs = useSelector(
+    (state: RootState) => state.selectedVerbs.selectedVerbs,
+  );
+  const [filteredVerbs, setFilteredVerbs] = useState<string[]>([]);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const SpanishVerbs = require("spanish-verbs");
+    console.log(SpanishVerbs.validTenses);
+  }, []);
+
+  const selectVerb = (verb: string) => {
+    console.log(verb);
+    if (!isVerbAdded(verb)) {
+      ga.event("chose_verbs", {
+        category: "via_full_list",
+        label: "select verb",
+        attempted_verb: verb,
+      });
+      dispatch(addVerb(verb));
+    } else {
+      removeVerbWrap(verb);
+    }
+  };
+
+  const removeVerbWrap = (verb: string) => {
+    dispatch(removeVerb(verb));
+  };
+
+  const isVerbAdded = (verb: string) => selectedVerbs.includes(verb);
+
+  const goPractice = () => {
+    router.push(PagesUrl.tense);
+  };
+
+  return (
+    <main className="flex min-h-screen flex-col items-center lg:p-24 lg:pt-32 lg:pb-48 pb-32 pt-8">
+      <h1 className="text-2xl md:text-3xl font-bold mb-4 md:mb-12 text-center text-gray-800 pt-12 md:pt-0">
+        Select verbs to practice ({Dictionary.length})
+      </h1>
+      <ChooseStepsDescription />
+      {selectedVerbs.length > 0 && (
+        <div className="mb-6 bg-white shadow-lg p-6 pb-4">
+          <h2 className="text-xl text-center font-semibold text-gray-700 mb-6">
+            Currently selected
+          </h2>
+          <div className="inline-flex flex-wrap items-center ">
+            {selectedVerbs.map((item, index) => (
+              <ChipItem
+                key={`selectedVerbsMap${index}`}
+                callback={removeVerbWrap}
+                item={item}
+              />
+            ))}
+
+            <button
+              onClick={() => goPractice()}
+              className="ml-4 px-6 py-2 mb-2 bg-green-500 text-white font-bold shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50 transition"
+            >
+              Next
+            </button>
+            <ClearAllVerbs />
+          </div>
+        </div>
+      )}
+      <div className="mb-8">
+        <div className="lg:min-w-[540px]">
+          <UiCombobox
+            options={Dictionary}
+            isMultiple={true}
+            showSelectedChips={false}
+            minSearchLength={2}
+            updateFilteredOptions={setFilteredVerbs}
+            onChange={(verbs: string[]) => dispatch(setSelectedVerbs(verbs))} // Update selected verbs
+            selectedOptions={selectedVerbs} // Pass the current selected verbs
+            label="Find a verb by query"
+          />
+        </div>
+      </div>
+      <div className="mb-8">
+        <VerbsPresets />
+      </div>
+
+      <h4 className="mb-4 block text-xl font-medium text-gray-700">
+        List of all verbs
+      </h4>
+      <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4">
+        {(filteredVerbs?.length ? filteredVerbs : Dictionary).map(
+          (item, index) => (
+            <li
+              key={`Dictionary_${index}`}
+              className="p-2 lg:p-4 bg-gray-500 shadow-md flex items-center cursor-pointer text-white"
+              onClick={() => selectVerb(item)}
+            >
+              <div>
+                <input
+                  type="checkbox"
+                  checked={selectedVerbs.includes(item)}
+                  readOnly
+                  className="h-5 w-5 mr-2 text-blue-600 bg-white border-gray-300 focus:ring-2 focus:ring-blue-500"
+                />
+              </div>
+              {item}
+            </li>
+          ),
+        )}
+      </ul>
+      <FooterAction
+        selectedOptions={selectedVerbs}
+        emptyText="Choose verbs you would like to practice"
+        btnText="Next"
+        mainAction={goPractice}
+      />
+    </main>
+  );
+}
---
+++ src/app/practice-selected/PracticeSelectedClient.tsx
@@ -0,0 +1,268 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CheckForm from "@/components/CheckForm";
+import { RootState } from "@/types/typeVerbs";
+import Pronomb from "@/data/pronomb";
+import Tense from "@/data/tense";
+import { useRouter } from "next/navigation";
+import getRandomInRange from "@/util/getRandom";
+import getKeyByValue from "@/util/getKeyByValue";
+import ChipItem from "@/components/ChipItem";
+import ClearAllVerbs from "@/components/ClearAllVerbs";
+import FooterContact from "@/components/FooterContact";
+import { PagesUrl } from "@/data/urls";
+import { removeVerb } from "@/store/modules/selectedVerbsSlice";
+import { removeTense } from "@/store/modules/selectedTensesSlice";
+import { AnswerResults } from "@/types/typeAnswers";
+import * as ga from "@/plugins/ga";
+
+export default function PracticeSelected() {
+  const router = useRouter();
+  const SpanishVerbs = require("spanish-verbs");
+  const dispatch = useDispatch();
+  const selectedVerbs = useSelector(
+    (state: RootState) => state.selectedVerbs.selectedVerbs,
+  );
+
+  const selectedTenses = useSelector(
+    (state: RootState) => state.selectedTenses.selectedTenses,
+  );
+
+  const [answerResults, setAnswerResults] = useState<AnswerResults>({
+    correct: 0,
+    wrong: 0,
+  });
+  const [currentPronomb, setCurrentPronomb] = useState("");
+  const [currentVerb, setCurrentVerb] = useState("");
+  const [currentTense, setCurrentTense] = useState("");
+  const [resetKey, setResetKey] = useState(0);
+  const [useSpecialCharacters, setUseSpecialCharacters] = useState(true);
+
+  function getCurrentConjugated() {
+    let conjugatedVerb = "";
+    if (currentVerb && currentTense) {
+      const tenseKey = getKeyByValue(Tense, currentTense);
+      conjugatedVerb = SpanishVerbs.getConjugation(
+        currentVerb,
+        tenseKey,
+        Pronomb.indexOf(currentPronomb),
+      );
+    }
+
+    return conjugatedVerb;
+  }
+
+  function randomiseVerbsAndTense(isCorrect: boolean) {
+    if (resetKey > 0) {
+      updateAnswerResults(isCorrect);
+    } else {
+      setResetKey((prevKey) => prevKey + 1);
+    }
+
+    setCurrentVerb(
+      selectedVerbs.length > 1
+        ? selectedVerbs[getRandomInRange(0, selectedVerbs.length - 1)]
+        : selectedVerbs[0],
+    );
+    setCurrentTense(
+      selectedTenses.length > 1
+        ? selectedTenses[getRandomInRange(0, selectedTenses.length - 1)]
+        : selectedTenses[0],
+    );
+    setCurrentPronomb(Pronomb[getRandomInRange(0, 5)]);
+  }
+
+  const goToVerbsList = () => {
+    router.push(PagesUrl.verbs);
+  };
+
+  const goToTensesList = () => {
+    router.push(PagesUrl.tense);
+  };
+
+  const nothingSelected =
+    selectedVerbs.length === 0 && selectedTenses.length === 0;
+
+  const removeVerbWrap = (verb: string) => {
+    dispatch(removeVerb(verb));
+  };
+
+  const removeTenseWrap = (tense: string) => {
+    dispatch(removeTense(tense));
+  };
+
+  const updateAnswerResults = (isCorrect: boolean) => {
+    setAnswerResults((prev) => ({
+      correct: isCorrect ? prev.correct + 1 : prev.correct,
+      wrong: isCorrect ? prev.wrong : prev.wrong + 1,
+    }));
+
+    setResetKey((prevKey) => prevKey + 1);
+  };
+
+  useEffect(() => {
+    randomiseVerbsAndTense(false);
+    ga.event("load_page", {
+      category: "practice_selected",
+    });
+  }, []);
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center px-4 lg:p-12 pt-24 lg:pt-32 pb-44 lg:pb-4">
+      {selectedVerbs.length > 0 && selectedTenses.length > 0 && (
+        <div className="p-8 bg-white shadow-lg w-full mb-2">
+          <div>
+            <div className="mb-4">
+              <div className="mb-4 text-3xl font-bold text-gray-800 text-center">
+                Conjugate this
+              </div>
+              <div className="text-xl text-gray-800">
+                {currentPronomb}{" "}
+                <i className="text-gray-800">
+                  <b>({currentVerb})</b>
+                </i>{" "}
+                in {currentTense}
+              </div>
+            </div>
+
+            <CheckForm
+              key={`check_form-${resetKey}`}
+              correctAnswer={getCurrentConjugated()}
+              goToNext={randomiseVerbsAndTense}
+              useSpecialCharacters={useSpecialCharacters}
+              setUseSpecialCharacters={setUseSpecialCharacters}
+              answerResults={answerResults}
+              answerCounter={resetKey}
+            ></CheckForm>
+          </div>
+        </div>
+      )}
+      <div className="mt-2 flex flex-col gap-4 lg:mb-6 mb-2">
+        <div className="flex w-full max-w-xs justify-between rounded-2xl px-6 py-2">
+          <div className="text-center px-2">
+            <p className="text-sm text-gray-500">✅ Correct</p>
+            <p className="text-lg font-semibold text-green-600">
+              {answerResults?.correct}
+            </p>
+          </div>
+          <div className="text-center px-2">
+            <p className="text-sm text-gray-500">❌ Wrong</p>
+            <p className="text-lg font-semibold text-red-500">
+              {answerResults?.wrong}
+            </p>
+          </div>
+          <div className="text-center px-2">
+            <p className="text-sm text-gray-500">🧮 Total </p>
+            <p className="text-lg font-semibold text-blue-500">
+              {resetKey ? resetKey - 1 : 0}
+            </p>
+          </div>
+        </div>
+      </div>
+      {!nothingSelected && (
+        <div className="lg:flex mb-8 lg:mb-24">
+          <div className="group lg:me-6 mb-6 flex-1">
+            <div className="p-6 bg-white shadow-lg">
+              {selectedVerbs.length === 0 ? (
+                <div>
+                  <div className="text-xl text-gray-800">
+                    Good, <b>but not enough!</b> I'll still ask you to choose
+                    verbs as well
+                    <div className="mt-4">
+                      <button
+                        className="bg-yellow-500 hover:bg-yellow-600 text-beige font-bold shadow-md px-8 py-4 cursor-pointer"
+                        onClick={() => goToVerbsList()}
+                      >
+                        Select verbs
+                      </button>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div className="mb-4 text-xl font-bold text-gray-800">
+                    Verbs selected to practice:{" "}
+                  </div>
+                  <div className="flex flex-wrap items-center gap-2 mb-4">
+                    {selectedVerbs.map((item, index) => (
+                      <ChipItem
+                        key={`selectedVerbs_practice_item_${index}`}
+                        callback={removeVerbWrap}
+                        item={item}
+                      />
+                    ))}
+                    <ClearAllVerbs />
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="group lg:ms-6 flex-1">
+            <div className="p-6 bg-white shadow-lg">
+              {selectedTenses.length === 0 ? (
+                <div className="text-xl text-gray-800">
+                  Good, <b>but not enough!</b> I'll still ask you to choose the
+                  tenses as well
+                  <div className="mt-4">
+                    <button
+                      className="bg-yellow-500 hover:bg-yellow-600 text-beige font-bold shadow-md px-8 py-4 cursor-pointer"
+                      onClick={() => goToTensesList()}
+                    >
+                      Select tenses
+                    </button>
+                  </div>
+                </div>
+              ) : (
+                <div>
+                  <div className="mb-4 text-xl font-bold text-gray-800">
+                    Tenses selected to practice:{" "}
+                  </div>
+                  <div className="flex flex-wrap gap-2">
+                    {selectedTenses.map((item, index) => (
+                      <ChipItem
+                        callback={removeTenseWrap}
+                        item={item}
+                        key={`selectedTensesPratctice_item_${index}`}
+                      />
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      )}
+      {nothingSelected && (
+        <div>
+          <div className="group">
+            <div className="p-6 bg-white shadow-lg flex flex-col items-center transition-transform transform hover:scale-105 -mt-32">
+              <h2 className="text-xl font-semibold text-gray-700 mb-6">
+                Riding too fast, amigo!
+              </h2>
+              <div className="text-gray-600 text-center">
+                To start testing your conjugation skills please select{" "}
+                <b>verbs</b> first. <br />
+                Then to really to practice you'll have to chose <b>tenses</b> as
+                well . <br />
+                <br />
+                Both steps are mandatories!! You little cheater!
+                <div className="mt-4">
+                  <button
+                    className="bg-yellow-500 hover:bg-yellow-600 text-beige font-bold shadow-md px-6 py-2"
+                    onClick={() => goToVerbsList()}
+                  >
+                    Select verbs
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+      <FooterContact />
+    </div>
+  );
+}
