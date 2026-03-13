70d1195fcb1ddc1318ddc58375a08c9fc8895e98
randomhero177 <randomhero177@gmail.com>
2026-03-13 18:56:56 +0100

    [cme-54]: set only 3 random tenses, set proper styles

--- src/app/practice-selected/RandomVerbs.tsx
+++ src/app/practice-selected/RandomVerbs.tsx
@@ -5,33 +5,37 @@ import Tense from "@/data/tense";
 import { setSelectedTenses } from "@/store/modules/selectedTensesSlice";
 import { setSelectedVerbs } from "@/store/modules/selectedVerbsSlice";
 type RandomVerbsProps = {
-  mainAction: () => void;
+  mainAction: (verbs: string[], tenses: string[]) => void;
 };
 export default function RandomVerbs({ mainAction }: RandomVerbsProps) {
   const dispatch = useDispatch();
   let verbsList: string[] = [];
+  let tensesList: string[] = [];
   const existingTenses = Object.values(Tense);
 
   function getRandomVerbs() {
-    console.log("getRandomVerbs");
-    console.log(getRandomInRange(0, Dictionary.length - 1));
-
     for (let i = 0; i < 10; ++i) {
       const randomIndex = getRandomInRange(0, Dictionary.length - 1);
       verbsList.push(Dictionary[randomIndex]);
     }
 
-    console.log(existingTenses);
-    console.log(verbsList);
+    for (let i = 0; i < 3; ++i) {
+      const randomIndex = getRandomInRange(0, existingTenses.length - 1);
+      tensesList.push(existingTenses[randomIndex]);
+    }
+
     dispatch(setSelectedVerbs(verbsList));
-    dispatch(setSelectedTenses(existingTenses));
-    mainAction(verbsList, existingTenses);
+    dispatch(setSelectedTenses(tensesList));
+    mainAction(verbsList, tensesList);
   }
 
   return (
     <div>
       <div>
-        <button onClick={() => getRandomVerbs()}>
+        <button
+          onClick={() => getRandomVerbs()}
+          className="ml-4 px-6 py-2 bg-green-500 text-amber-50 font-bold shadow-md hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-400 focus:ring-opacity-50 transition"
+        >
           Practice 10 random verbs
         </button>
       </div>
