41f7438696c5e43c81a2ac6fcd2fea6e95064a5d
randomhero177 <randomhero177@gmail.com>
2026-06-02 20:26:34 +0200

    [cme-41]: add withPWAInit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+  reactStrictMode: false,
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
